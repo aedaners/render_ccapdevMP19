713e9d64c102364dacec175492dd949c84b993bc
aedaners <87559949+aedaners@users.noreply.github.com>
2022-12-07 20:50:47 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.get('/login', function(req, res) {
     res.sendFile(__dirname + "/login.html")
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server started on port 3000");
+});
 
 //Connect to MongoDB
 const { MongoClient, ServerApiVersion } = require('mongodb');
